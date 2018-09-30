d3b3d3c6f9278a70296dc5a3fb597603b17058bd
Karun Sekhar <31802738+karsekh21@users.noreply.github.com>
2018-09-30 19:45:40 -0400

    Add files via upload

--- gitStuff.py
+++ gitStuff.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python
 import json
 import requests
+
 
 def gitHubStuff(userID):
     try:
@@ -8,12 +10,11 @@ def gitHubStuff(userID):
         return "userID should be a string"
     repoList = []
     gitInfo = requests.get(urlString)
-    gitJSON = json.loads(gitCommits.content.decode('utf - 8'))
     for repo in gitJSON:
         gitCommits = requests.get("https://api.github.com/users/" + userID + "/" + repo['name'] + "/commits")
         gitJSON = json.loads(gitCommits.content.decode('utf-8'))
         ref = 0
         for commitItem in gitJSON:
             ref += 1
-        resultList.append([repo['name'], count])
+        resultList.append(repo['name'], ref)
     return resultList
